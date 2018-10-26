e0daedf639d85bd49501d437ec4f058663cd1336
Benji Olsen <benjaminalanolsen@gmail.com>
2018-10-26 20:37:35 +0800

    Neatened and fixed up the bare graph class

--- Graph.java
+++ Graph.java
@@ -1,33 +1,44 @@
 import java.util.*;
-public class Graph
-{
-    private class GraphVertex
-    {
+public class Graph{
+    private class GraphEdge{
+        private String label;
+        private double value;
+        private boolean visited;
+
+        public GraphEdge(String inLabel,double inValue){
+            label = inLabel;
+            value = inValue;
+        }
+
+        public String getLabel(){ return label; }
+        public double getValue(){ return value; }
+        public void getFrom(){}
+        public void getTo(){}
+
+    }
+
+    private class GraphVertex{
         private String label;
         private boolean visited;
+        private
         private DSALinkedList<GraphVertex> links;
 
-        private GraphVertex(String inLabel)
-        {
-            if(inLabel!=null)
-            {
+        private GraphVertex(String inLabel){
+            if(inLabel!=null){
                 label = inLabel;
             }
-            else
-            {
+            else{
                 throw new IllegalArgumentException("Unable to create vertex");
             }
         }
 
-        private String getLabel()
-        {
-            return label;
-        }
+        private String getLabel(){ return label; }
 
-        private DSALinkedList<GraphVertex> getAdjacent()
-        {
-            return links;
-        }
+        public boolean getVisited(){ return visited; }
+
+        private DSALinkedList<GraphVertex> getAdjacent(){ return links; }
+
+        public String toString(){ return label; }
 
         private void addEdge(GraphVertex inEdge)
         {
@@ -44,15 +55,7 @@ public class Graph
 
         }
 
-        public boolean getVisited()
-        {
-            return visited;
-        }
 
-        public String toString()
-        {
-            return label;
-        }
     }
     private DSALinkedList<GraphVertex> vertices;
 

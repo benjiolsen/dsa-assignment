2e290b4a0c7812f51aa46c71f1b10d27373e353b
Benji Olsen <benjaminalanolsen@gmail.com>
2018-10-28 21:11:12 +0800

    Improved the graph class

--- Graph.java
+++ Graph.java
@@ -1,62 +1,85 @@
+/*
+    STUDENT NUMBER: 19481681
+    NAME: Benjamin Olsen
+    UNIT: COMP1002 Data Structures and Algorithms
+    CLASS: Graph
+    PURPOSE: This class is used to act as a graph ADT for DSA and the
+             assignment. Storing information about edges, vertices and the
+             graph in general. Using an adjacency list to store each adjacent
+             node, and edges to store the weight and value for each vertex
+             connection
+*/
 import java.util.*;
 public class Graph{
     private class GraphEdge{
         private String label;
-        private double value;
+        private Object value;
         private boolean visited;
 
-        public GraphEdge(String inLabel,double inValue){
+        public GraphEdge(String inLabel,Object inValue){
             label = inLabel;
             value = inValue;
         }
 
-        public String getLabel(){ return label; }
-        public double getValue(){ return value; }
-        public void getFrom(){}
-        public void getTo(){}
-
+        public static String getLabel(){ return label; }
+        public static Object getValue(){ return value; }
+        public static GraphVertex getFrom(){}
+        public static GraphVertex getTo(){}
+        public static boolean isDirected(){}
+        public static String toString(){}
     }
 
     private class GraphVertex{
         private String label;
-        private boolean visited;
         private Object value;
+        private boolean visited;
         private LinkedList<GraphVertex> links;
 
-        private GraphVertex(String inLabel){
+        private GraphVertex(String inLabel,Object inValue){
             if(inLabel!=null){
                 label = inLabel;
             }
             else{
-                throw new IllegalArgumentException("Unable to create vertex");
+                throw new IllegalArgumentException("Invalid Label");
             }
+            value = inValue;
         }
 
-        private String getLabel(){ return label; }
+        private static String getLabel(){return label;}
+        public static boolean getVisited(){return visited;}
+        public static void setVisited(){visited = true;}
+        public static void clearVisited(){visited = false;}
+        private static LinkedList<GraphVertex> getAdjacent(){ return links; }
+        public static boolean isSource(){}
+        public static boolean isTarget(){}
 
-        public boolean getVisited(){ return visited; }
-
-        private LinkedList<GraphVertex> getAdjacent(){ return links; }
-
-        public String toString(){ return label; }
-
-        private void addEdge(GraphVertex inEdge)
-        {
-
+        private static void addEdge(GraphVertex inEdge){
+            links.insertFirst(inEdge);
         }
 
-        public void setVisited()
-        {
-
         }
-
-        public void clearVisited()
-        {
-
-        }
-
-
+        public static String toString(){ return label; }
     }
     private LinkedList<GraphVertex> vertices;
+    private LinkedList<GraphEdge> edges;
 
+    public graph(){
+        vertices = new LinkedList<GraphVertex>();
+        edges = new LinkedList<GraphEdge>();
+    }
+    public static void addVertex(String label,Object value){
+
+    }
+    public static void addEdge(String label1,String label2){
+
+    }
+    public static int getVertexCount(){}
+    public static int getEdgeCount(){}
+    public static GraphVertex getVertex(String inLabel){}
+    public static GraphEdge getVertex(String inLabel){}
+    public static LinkedList<GraphVertex> adjacent(GraphVertex vert){}
+    public static LinkedList<GraphEdge> adjacentE(GraphVertex vert){}
+    public static int degree(GraphVertex vert){}
+    public static boolean hasValue(Object inValue){}
+    public boolean isAdjacent(String label1, String label2){}
 }
